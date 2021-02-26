329bec98166fbb1ae818a0e4bae8945bdf7065e8
denzven <denzvenvadakkan@gmail.com>
2021-02-26 13:50:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
+from asyncio import events
 import discord
 from discord.ext import commands
 import os
 
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="+")
 
 @bot.event
 async def on_ready():
     print("the bot is ready")
     
+@bot.event
+async def on_member_join(member):
+    print(f"{member} has joined a server")
+    
+@bot.event
+async def on_member_leave(member):
+    print(f"{member} has left a server")
+
+    
 @bot.command()
 async def test(ctx):
     await ctx.send("Test")
 
-bot.run(os.get.env("BOTTOKEN"))
+#bot.run(os.getenv("BOTTOKEN"))
+bot.run("ODEzOTk5MjE5NDI0NjI0NjQw.YDXd2Q.wmCHUz2btIRESPvtIdW3a6Z2IC8")
