2acf4113b02912612966d1b1f98895b451a8e06a
denzven <denzvenvadakkan@gmail.com>
2021-02-26 15:14:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,7 @@ from asyncio import events
 import discord
 from discord.ext import commands
 import os
-
-
+import random
 
 bot = commands.Bot(command_prefix="+")
 
@@ -27,6 +26,29 @@ async def ping(ctx):
 async def test(ctx):
     await ctx.send("test")
     
+@bot.command(aliases = [ '8ball' , '8b' ])
+async def _8ball(ctx,*,question):
+    responses = ["It is certain.",
+                 "It is decidedly so.",
+                 "Without a doubt.",
+                 "Yes - definitely.",
+                 "You may rely on it.",
+                 "As I see it, yes.",
+                 "Most likely.",
+                 "Outlook good.",
+                 "Yes.",
+                 "Signs point to yes.",
+                 "Reply hazy, try again.",
+                 "Ask again later.",
+                 "Better not tell you now.",
+                 "Cannot predict now.",
+                 "Concentrate and ask again.",
+                 "Don't count on it.",
+                 "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.",
+                 "Very doubtful."]
+    await ctx.send(f"question :{question} \nAnswer: {random.choice(responses)}")
 
 #bot.run(os.getenv("BOTTOKEN"))
 bot.run("ODEzOTk5MjE5NDI0NjI0NjQw.YDXd2Q.wmCHUz2btIRESPvtIdW3a6Z2IC8")
